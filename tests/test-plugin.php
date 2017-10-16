d44855e4cc55e70a19922209dc371cebdf8eebb7
selu91 <cristea.marius.alexandru@gmail.com>
2017-10-16 12:45:01 +0300

    Added new deployment stack.

---
+++ tests/test-plugin.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * WordPress unit test plugin.
+ *
+ * @package     LoginCustomizer
+ * @subpackage  Tests
+ * @copyright   Copyright (c) 2017, Marius Cristea
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since 3.0.10
+ */
+class Test_LoginCustomizer extends WP_UnitTestCase {
+
+	public function generic() {
+		$this->assertTrue();
+	}
+}
